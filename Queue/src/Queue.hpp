15980d2c3a340be23a1b419f26f3f4e40eeb3ab4
Reza Panahi <rezapanahiads2@gmail.com>
2022-06-07 19:12:55 +0430

    implement queue data structure

--- Queue/src/Queue.hpp
+++ Queue/src/Queue.hpp
@@ -1,11 +1,67 @@
-#include <vector>
+#pragma once
+#include <iostream>
+#include "Node.hpp"
 
 template <typename T>
 class Queue
 {
-	std::vector<T> data;
+	Node<T>* Head;
+	Node<T>* Tail;
 
 public:
-	Queue() = default;
-	~Queue() = default;
+	Queue()
+	{
+		Head = nullptr;
+		Tail = nullptr;
+	}
+	~Queue()
+	{
+		if (Head != nullptr)
+		{
+			delete Head;
+		}
+	}
+
+	void print()
+	{
+		Node<T>* temp = Head;
+		while (temp != nullptr)
+		{
+			std::cout << temp->data << std::endl;
+			temp = temp->next;
+		}
+	}
+
+	void push(const T& value)
+	{
+		Node<T>* node = new Node<T>(value);
+		if (Head == nullptr)
+		{
+			Head = Tail = node;
+		}
+		else
+		{
+			Tail->next = node;
+			Tail = node;
+		}
+	}
+
+	void pop()
+	{
+		if (Head != nullptr)
+		{
+			if (Head == Tail)
+				Tail == nullptr;
+			Node<T>* temp = Head;
+			Head = Head->next;
+
+			temp->next = nullptr;
+			delete temp;
+		}
+	}
+
+	const T& front()
+	{
+		return Head->data;
+	}
 };
