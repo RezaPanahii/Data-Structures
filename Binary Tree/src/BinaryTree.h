c8a0fe83c9e57c0bea084fb3fe8837772227b04f
Reza Panahi <rezapanahiads2@gmail.com>
2022-06-13 08:24:39 +0430

    add height function

--- Binary Tree/src/BinaryTree.h
+++ Binary Tree/src/BinaryTree.h
@@ -112,3 +112,14 @@ Node* levelOrderBuild()
 	}
 	return root;
 }
+
+int height(Node* root)
+{
+    if (root == nullptr)
+        return 0;
+
+    int left_height = height(root->left);
+    int right_height = height(root->right);
+
+    return std::max(left_height, right_height) + 1;
+}
